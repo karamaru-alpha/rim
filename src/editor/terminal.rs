d066e4e1cd6ee5b3c34e0bd27554e5a8f495846b
karamaru-alpha <mrnk3078@gmail.com>
2024-12-21 23:17:03 +0900

    feat: text viwer

--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -1,73 +1,113 @@
-use core::fmt::Display;
 use crossterm::cursor::{Hide, MoveTo, Show};
 use crossterm::style::Print;
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode, size, Clear, ClearType};
+use crossterm::terminal::{
+    disable_raw_mode, enable_raw_mode, size, Clear, ClearType, EnterAlternateScreen,
+    LeaveAlternateScreen,
+};
 use crossterm::{queue, Command};
 use std::io::{stdout, Error, Write};
 
-pub struct Terminal {}
-
-#[derive(Clone, Copy)]
-pub struct Position {
-    pub row: usize,
-    pub col: usize,
-}
-
-#[derive(Clone, Copy)]
+#[derive(Default, Copy, Clone)]
 pub struct Size {
     pub height: usize,
     pub width: usize,
 }
+#[derive(Copy, Clone, Default)]
+pub struct Position {
+    pub col: usize,
+    pub row: usize,
+}
+
+impl Position {
+    pub const fn saturating_sub(self, other: Self) -> Self {
+        Self {
+            row: self.row.saturating_sub(other.row),
+            col: self.col.saturating_sub(other.col),
+        }
+    }
+}
+
+pub struct Terminal;
 
 impl Terminal {
+    pub fn terminate() -> Result<(), Error> {
+        Self::leave_alternate_screen()?;
+        Self::show_caret()?;
+        Self::execute()?;
+        disable_raw_mode()?;
+        Ok(())
+    }
+
     pub fn initialize() -> Result<(), Error> {
         enable_raw_mode()?;
+        Self::enter_alternate_screen()?;
         Self::clear_screen()?;
-        Self::execute()
-    }
-
-    pub fn terminate() -> Result<(), Error> {
-        disable_raw_mode()
-    }
-
-    pub fn size() -> Result<Size, Error> {
-        let (width, height) = size()?;
-        Ok(Size {
-            height: height as usize,
-            width: width as usize,
-        })
+        Self::execute()?;
+        Ok(())
     }
 
     pub fn clear_screen() -> Result<(), Error> {
-        Self::queue(Clear(ClearType::All))
+        Self::queue_command(Clear(ClearType::All))?;
+        Ok(())
     }
 
     pub fn clear_line() -> Result<(), Error> {
-        Self::queue(Clear(ClearType::CurrentLine))
+        Self::queue_command(Clear(ClearType::CurrentLine))?;
+        Ok(())
     }
 
-    pub fn move_caret_to(position: Position) -> Result<(), std::io::Error> {
-        #[allow(clippy::as_conversions, clippy::cast_possible_truncation)]
-        Self::queue(MoveTo(position.col as u16, position.row as u16))
+    pub fn move_caret_to(position: Position) -> Result<(), Error> {
+        #[allow(clippy::cast_possible_truncation, clippy::cast_possible_truncation)]
+        Self::queue_command(MoveTo(position.col as u16, position.row as u16))?;
+        Ok(())
     }
 
-    pub fn show_caret() -> Result<(), Error> {
-        Self::queue(Show)
+    pub fn enter_alternate_screen() -> Result<(), Error> {
+        Self::queue_command(EnterAlternateScreen)?;
+        Ok(())
+    }
+
+    pub fn leave_alternate_screen() -> Result<(), Error> {
+        Self::queue_command(LeaveAlternateScreen)?;
+        Ok(())
     }
 
     pub fn hide_caret() -> Result<(), Error> {
-        Self::queue(Hide)
+        Self::queue_command(Hide)?;
+        Ok(())
     }
 
-    pub fn print<T: Display>(v: T) -> Result<(), Error> {
-        Self::queue(Print(v))
+    pub fn show_caret() -> Result<(), Error> {
+        Self::queue_command(Show)?;
+        Ok(())
     }
 
-    fn queue<T: Command>(command: T) -> Result<(), Error> {
-        queue!(stdout(), command)
+    pub fn print(string: &str) -> Result<(), Error> {
+        Self::queue_command(Print(string))?;
+        Ok(())
+    }
+
+    pub fn print_row(row: usize, line_text: &str) -> Result<(), Error> {
+        Self::move_caret_to(Position { row, col: 0 })?;
+        Self::clear_line()?;
+        Self::print(line_text)?;
+        Ok(())
+    }
+
+    pub fn size() -> Result<Size, Error> {
+        let (width_u16, height_u16) = size()?;
+        let height = height_u16 as usize;
+        let width = width_u16 as usize;
+        Ok(Size { height, width })
     }
 
     pub fn execute() -> Result<(), Error> {
-        stdout().flush()
+        stdout().flush()?;
+        Ok(())
+    }
+
+    fn queue_command<T: Command>(command: T) -> Result<(), Error> {
+        queue!(stdout(), command)?;
+        Ok(())
     }
 }
